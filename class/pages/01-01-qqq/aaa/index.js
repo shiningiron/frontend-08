3901ecaa1350dc75a1c16b694c67d10466b6d279
heechul <shiningiron94@gmail.com>
2022-07-04 16:50:36 +0900

    init

---
+++ class/pages/01-01-qqq/aaa/index.js
@@ -0,0 +1,13 @@
+export default function Mypage() {
+
+
+    //  여기는 자바스크립트 쓰는곳
+
+    return (
+        <div>
+            안녕하세요~!!
+            <input type="text"/>
+        </div>
+       
+    )
+}
