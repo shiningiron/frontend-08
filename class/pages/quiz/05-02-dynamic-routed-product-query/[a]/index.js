ce461cc8dfd279ebb8fe4e4de63cfd3ca2399c84
heechul <shiningiron94@gmail.com>
2022-07-10 01:31:27 +0900

    practice

--- class/pages/quiz/05-02-dynamic-routed-product-query/[a]/index.js
+++ class/pages/quiz/05-02-dynamic-routed-product-query/[a]/index.js
@@ -1,5 +1,5 @@
 import { useQuery, gql } from "@apollo/client";
-import loadConfig from "next/dist/server/config";
+
 import { useRouter } from "next/router";
 
 const FETCH_PRODUCT = gql`
@@ -29,7 +29,7 @@ export default function DynamicRoutedPage () {
             <div>{data?.fetchProduct._id}</div>
             <div>판매자: {data && data.fetchProduct.seller}</div>
             <div>상품명: {data?.fetchProduct.name}</div>
-            {/* <div>상품내용: {data ? data.fetchProduct.detail : "loading"} </div> */}
+            <div>상품내용: {data ? data.fetchProduct.detail : "loading"} </div>
             <div>상품가격: </div>
         </>
     )
